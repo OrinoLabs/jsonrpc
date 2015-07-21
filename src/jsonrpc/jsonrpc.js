ecd4bbb2d8cbcc5000b6517a2e3090d51bdb2a1e
Michael Buerge <mib@orino.ch>
2015-07-21 14:46:58 +0200

    Introduce call options. Add retry logic.

--- src/jsonrpc/jsonrpc.js
+++ src/jsonrpc/jsonrpc.js
@@ -17,6 +17,15 @@ goog.require('jsonrpc.Error');
 
 
 /**
+ * @typedef {{
+ *   maxAttempts: number,
+ *   shouldRetry: function(jsonrpc.Error):boolean,
+ * }}
+ */
+jsonrpc.CallOptions;
+
+
+/**
  * @type {jsonrpc.Transport}
  */
 jsonrpc.defaultTransport;
@@ -25,14 +34,50 @@ jsonrpc.defaultTransport;
 /**
  * @param {string} method
  * @param {Object=} opt_params
+ * @param {jsonrpc.CallOptions=} opt_opts
  * @return {goog.Promise<Object>}
  */
-jsonrpc.call = function(method, opt_params) {
+jsonrpc.call = function(method, opt_params, opt_opts) {
+  var opts = opt_opts || {};
+  console.log(opts)
   var transport = jsonrpc.defaultTransport;
   if (!transport) {
     throw new Error('No transport specified.');
   }
 
+  var maxAttempts = opts.maxAttempts || 1;
+  var attempt = 0;
+
+  return new goog.Promise(function(resolve, reject) {
+    function attemptCall() {
+      attempt += 1;
+
+      jsonrpc.performCall_(transport, method, opt_params)
+      .then(function(result) {
+        resolve(result);
+      })
+      .then(null, function(err) {
+        if (attempt == maxAttempts) {
+          reject(err);
+        } else if (opts.shouldRetry && !opts.shouldRetry(err)) {
+          reject(err);
+        } else {
+          attemptCall();
+        }
+      });
+    }
+
+    attemptCall();
+  })
+};
+
+
+/**
+ * @param {jsonrpc.Transport} transport
+ * @parma {string} method
+ * @param {Object=} opt_params
+ */
+jsonrpc.performCall_ = function(transport, method, opt_params) {
   return new goog.Promise(function(resolve, reject) {
     transport.performCall(method, opt_params)
     .then(function(responseJson) {
@@ -56,7 +101,6 @@ jsonrpc.call = function(method, opt_params) {
       reject(error);
     });
   });
-};
-
+}
 
 
