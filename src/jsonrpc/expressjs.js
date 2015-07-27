7e6e0abafbc9abcfd075af1b740df95e7a7b8f01
Michael Buerge <mib@orino.ch>
2015-07-27 22:22:28 +0200

    jsonrpc.expressjs.createHandler()

---
+++ src/jsonrpc/expressjs.js
@@ -0,0 +1,73 @@
+// Copyright 2015 Michael Bürge <mib@orino.ch>
+// All rights reserved.
+
+
+goog.provide('jsonrpc.expressjs');
+
+
+/**
+ * @param {jsonrpc.Dispatcher} dispatcher
+ * @param {function(express.Request, express.Response, jsonrpc.Error)=} opt_logError
+ * @return {function(express.Request, express.Response)}
+ */
+jsonrpc.expressjs.createHandler = function(dispatcher, opt_logError) {
+  
+  /**
+   * @param {express.Request} request
+   * @param {express.Response} response
+   */
+  var handlerFunction = function(httpRequest, httpResponse) {
+
+    function sendResponse(jsonrpcResponse) {
+      httpResponse.setHeader('Content-Type', 'application/json; harset=utf-8');
+      httpResponse.write(JSON.stringify(jsonrpcResponse));
+      httpResponse.end();
+    }
+    /**
+     * @param {*} result
+     */
+    function replyWithResult(result) {
+      sendResponse({'result': result});
+    }
+    /** 
+     * @param {jsonrpc.Error} error
+     */
+    function replyWithError(error) {
+      sendResponse({'error': error});
+    }
+
+    var requestData = httpRequest.body;
+
+    if (typeof requestData == 'string') {
+      try {
+        requestData = JSON.parse(requestData);
+      } catch (err) {
+        replyWithError(new jsonrpc.Error(jsonrpc.ErrorCode.PARSE_ERROR));
+        return;
+      }
+    } else if (typeof requestData == 'object') {
+      // Nothing to do.
+      // With the body-parser middleware in place and the appropriate
+      // content-type (application/json), the JSON parsing will already have
+      // been taken care of.
+    } else {
+      replyWithError(new jsonrpc.Error(jsonrpc.ErrorCode.INTERNAL_ERROR));
+      return;
+    }
+
+    var methodName = requestData['method'];
+    var params = requestData['params'] || {};
+
+    if (!methodName) {
+      replyWithError(new jsonrpc.Error(jsonrpc.ErrorCode.INVALID_REQUEST));
+      return;
+    }
+
+    dispatcher.dispatchCall(methodName, params)
+    .then(replyWithResult, replyWithError);
+  };
+
+  return handlerFunction;
+};
+
+
