14d0340c1f8f27b73d3cc6aff9f31292e44ee902
Michael Buerge <mib@orino.ch>
2017-04-24 18:28:06 +0200

    Get calls to work (headers field is not a simple array, but an instance of Headers).

--- src/jsonrpc/transport/angulartransport.js
+++ src/jsonrpc/transport/angulartransport.js
@@ -5,6 +5,9 @@ goog.provide('jsonrpc.AngularTransport');
 goog.require('jsonrpc.Transport');
 
 
+// TODO: Figure out how to properly get access to the Headers constructor.
+// Currently expecting it to be passed in to the constructor, which is a hack.
+
 
 /**
  * @param {Http} ngHttp
@@ -12,12 +15,18 @@ goog.require('jsonrpc.Transport');
  * @constructor
  * @implements {jsonrpc.Transport}
  */
-jsonrpc.AngularTransport = function(ngHttp, opt_endpointPath) {
+jsonrpc.AngularTransport = function(ngHttp, ngHeadersCtor, opt_endpointPath) {
   /**
    * @type {Http}
    * @private
    */
   this.ngHttp_ = ngHttp;
+
+  /**
+   * @type {function(): Headers}
+   * @private
+   */
+  this.ngHeadersCtor_ = ngHeadersCtor;
 
   /**
    * @type {string}
@@ -32,14 +41,20 @@ jsonrpc.AngularTransport.prototype.performCall = function(callId, method, params
   return new Promise(function(resolve, reject) {
     var body = JSON.stringify(
         {'id': callId, 'method': method, 'params': params});
+    var headers = new this.ngHeadersCtor_;
+    headers.set('Content-Type', 'application/json; charset=utf-8');
     /** @type {RequestOptionsArgs} */
     var opts = {
-      'headers': ['Content-Type: application/json; charset=utf-8]'],
+      'headers': headers,
     };
     this.ngHttp_
-      .post(this.endpointPath_, body)
-      .subscribe((res) => {
-        console.log(res);
-      });
+      .post(this.endpointPath_, body, opts)
+      .subscribe(
+        (res) => {
+          var jsonrpcResult = res.json();
+          resolve(jsonrpcResult);
+        },
+        (err) => reject(e));
+
   }.bind(this));
 };
